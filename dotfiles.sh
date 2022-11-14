7fdbaaa8c051f2793142f9b0e64d491763d92371
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-11-14 10:51:00 +0100

    dotfiles: Update script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 # dotfiles manager
 
-files=("sway/config" "waybar/config" "waybar/style.css")
+files=("sway/config" "sway/sway-prop" "waybar/config" "waybar/style.css" "waybar/mediaplayer.py")
 
-for f in "${files[@]}"
-do
-   echo "Updating $f"
-   cp ~/.config/$f $f
-   git add "$f"
-done
+if [ "$1" == "deploy" ]; then
+   echo "WARNING: This will overwrite your local files!"
 
-git commit -m "dotfiles: Automated sync"
-git push
+   for i in {5..1}; do
+      printf "\033[0K\rProceeding in $i..."
+      sleep 1
+   done
+
+   printf "\n"
+
+   for f in "${files[@]}"
+   do
+      echo "Deploying $f"
+      cp $f ~/.config/$f
+      sleep 0.25
+   done
+elif [ "$1" == "update" ]; then
+   for f in "${files[@]}"
+   do
+      echo "Updating $f"
+      cp ~/.config/$f $f
+   done
+elif [ "$1" == "push" ]; then
+   for f in "${files[@]}"
+   do
+      echo "Adding $f"
+      git add "$f"
+   done
+
+   git commit -m "dotfiles: Automated sync"
+   git push
+fi
